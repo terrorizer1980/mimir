0a7a5fd0628c272335f3b22ac68de62decfafe22
Dimitar Dimitrov <dimitar.dimitrov@grafana.com>
2022-04-13 13:35:33 +0200

    Update pkg/storage/bucket/client.go
    Co-authored-by: Marco Pracucci <marco@pracucci.com>

--- pkg/storage/bucket/client.go
+++ pkg/storage/bucket/client.go
@@ -85,7 +85,7 @@ func (cfg *Config) RegisterFlagsWithPrefixAndDefaultDirectory(prefix, dir string
 	cfg.Filesystem.RegisterFlagsWithPrefixAndDefaultDirectory(prefix, dir, f)
 
 	f.StringVar(&cfg.Backend, prefix+"backend", Filesystem, fmt.Sprintf("Backend storage to use. Supported backends are: %s.", strings.Join(cfg.supportedBackends(), ", ")))
-	f.StringVar(&cfg.StoragePrefix, prefix+"storage-prefix", "", fmt.Sprintf("Prefix for backend bucket/container. All files will be stored under this prefix."))
+	f.StringVar(&cfg.StoragePrefix, prefix+"storage-prefix", "", fmt.Sprintf("Prefix for all objects stored in the backend storage."))
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
